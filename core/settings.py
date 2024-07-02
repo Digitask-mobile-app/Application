dfdcae979e8a9364e962dfd7bab9684d17000c40
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-07-02 20:51:27 +0400

    image

--- core/settings.py
+++ core/settings.py
@@ -247,8 +247,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
