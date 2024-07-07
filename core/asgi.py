3ffd4dbb5d9fee52dc4bb60ca036f2e55a325b86
NicatDev <nicat254memmedov@gmail.com>
2024-07-08 03:36:26 +0400

    22

--- core/asgi.py
+++ core/asgi.py
@@ -6,8 +6,7 @@ import core.routing
 from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-import django
-django.setup()
+
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
