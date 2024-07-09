2d151a9be477276c9665555f2c36c8b3e6cf2ae3
NicatDev <nicat254memmedov@gmail.com>
2024-07-10 02:15:47 +0400

    22

--- core/routing.py
+++ core/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import StatusConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/status/$', StatusConsumer),
+    re_path(r'ws/status/$', StatusConsumer.as_asgi()),
 ]
