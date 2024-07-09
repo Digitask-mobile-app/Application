adf7fc274f6e2b88315e51135514f3a01613ee0f
NicatDev <nicat254memmedov@gmail.com>
2024-07-10 02:51:29 +0400

    22

--- core/asgi.py
+++ core/asgi.py
@@ -4,19 +4,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing  
 from channels.security.websocket import AllowedHostsOriginValidator
-from .consumers import StatusConsumer
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django_asgi_app = get_asgi_application()
-from django.urls import path
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-      "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter([
-                path("ws/status/", StatusConsumer.as_asgi()),
 
-            ])
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                core.routing.websocket_urlpatterns
+            )
         )
     ),
 })
