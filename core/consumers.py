8abe4fa4ccd775c64291005e4e3fcbdad6be0882
NicatDev <nicat254memmedov@gmail.com>
2024-07-09 20:01:15 +0400

    22

--- core/consumers.py
+++ core/consumers.py
@@ -1,58 +1,69 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
 import json
+from channels.generic.websocket import WebsocketConsumer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 from django.contrib.auth import get_user_model
-
+from channels.generic.websocket import AsyncWebsocketConsumer
 User = get_user_model()
 
-class StatusConsumer(AsyncWebsocketConsumer):
-    online_users = {}
 
+
+# class StatusConsumer(WebsocketConsumer):
+#     online_users = {}
+
+#     def connect(self):
+#         self.accept()
+
+#         self.user_id = self.scope['user'].id if self.scope['user'].is_authenticated else None
+#         if self.user_id:
+#             StatusConsumer.online_users[self.user_id] = self.channel_name
+#             self.update_user_status(self.user_id, True)
+#             self.broadcast_status(self.user_id, 'online')
+
+#     def disconnect(self, close_code):
+#         if self.user_id in StatusConsumer.online_users:
+#             del StatusConsumer.online_users[self.user_id]
+#             self.update_user_status(self.user_id, False)
+#             self.broadcast_status(self.user_id, 'offline')
+
+#     def update_user_status(self, user_id, online):
+#         try:
+#             user = User.objects.get(id=user_id)
+#             user.is_online = online
+#             user.save()
+#         except User.DoesNotExist:
+#             pass
+
+#     def receive(self, text_data):
+#         data = json.loads(text_data)
+#         user_id = data.get('userId')
+#         status = data.get('status')
+
+#         if user_id and status:
+#             self.broadcast_status(user_id, status)
+
+#     def broadcast_status(self, user_id, status):
+#         channel_layer = get_channel_layer()
+#         async_to_sync(channel_layer.group_send)(
+#             'status_updates',
+#             {
+#                 'type': 'user_status',
+#                 'user_id': user_id,
+#                 'status': status
+#             }
+#         )
+
+#     def user_status(self, event):
+#         self.send(text_data=json.dumps({
+#             'userId': event['user_id'],
+#             'status': event['status']
+#         }))
+
+class StatusConsumer(AsyncWebsocketConsumer):
+    
     async def connect(self):
         await self.accept()
-
-        self.user_id = self.scope['user'].id if self.scope['user'].is_authenticated else None
-        if self.user_id:
-            StatusConsumer.online_users[self.user_id] = self.channel_name
-            await self.update_user_status(self.user_id, True)
-            await self.broadcast_status(self.user_id, 'online')
-
+    
     async def disconnect(self, close_code):
-        if self.user_id in StatusConsumer.online_users:
-            del StatusConsumer.online_users[self.user_id]
-            await self.update_user_status(self.user_id, False)
-            await self.broadcast_status(self.user_id, 'offline')
+        pass  # Burada genellikle bağlantı sonlandırma işlemleri yapılır, ancak bu örnekte boş bırakıyoruz.
 
-    @database_sync_to_async
-    def update_user_status(self, user_id, online):
-        try:
-            user = User.objects.get(id=user_id)
-            user.is_online = online
-            user.save()
-        except User.DoesNotExist:
-            pass
-
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        user_id = data.get('userId')
-        status = data.get('status')
-
-        if user_id and status:
-            await self.broadcast_status(user_id, status)
-
-    async def broadcast_status(self, user_id, status):
-        channel_layer = self.channel_layer
-        await channel_layer.group_send(
-            'status_updates',
-            {
-                'type': 'user_status',
-                'user_id': user_id,
-                'status': status
-            }
-        )
-
-    async def user_status(self, event):
-        await self.send(text_data=json.dumps({
-            'userId': event['user_id'],
-            'status': event['status']
-        }))
