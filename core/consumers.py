fa5a1611a59e1212ac1e896651fca8bf73413784
NicatDev <nicat254memmedov@gmail.com>
2024-07-10 02:21:49 +0400

    22

--- core/consumers.py
+++ core/consumers.py
@@ -8,72 +8,57 @@ User = get_user_model()
 
 
 from asgiref.sync import sync_to_async
-# class StatusConsumer(WebsocketConsumer):
-#     online_users = {}
+class StatusConsumer(WebsocketConsumer):
+    online_users = {}
 
-#     def connect(self):
-#         self.accept()
+    def connect(self):
+        self.accept()
 
-#         self.user_id = self.scope['user'].id if self.scope['user'].is_authenticated else None
-#         if self.user_id:
-#             StatusConsumer.online_users[self.user_id] = self.channel_name
-#             self.update_user_status(self.user_id, True)
-#             self.broadcast_status(self.user_id, 'online')
+        self.user_id = self.scope['user'].id if self.scope['user'].is_authenticated else None
+        if self.user_id:
+            StatusConsumer.online_users[self.user_id] = self.channel_name
+            self.update_user_status(self.user_id, True)
+            self.broadcast_status(self.user_id, 'online')
 
-#     def disconnect(self, close_code):
-#         if self.user_id in StatusConsumer.online_users:
-#             del StatusConsumer.online_users[self.user_id]
-#             self.update_user_status(self.user_id, False)
-#             self.broadcast_status(self.user_id, 'offline')
+    def disconnect(self, close_code):
+        if self.user_id in StatusConsumer.online_users:
+            del StatusConsumer.online_users[self.user_id]
+            self.update_user_status(self.user_id, False)
+            self.broadcast_status(self.user_id, 'offline')
 
-#     def update_user_status(self, user_id, online):
-#         try:
-#             user = User.objects.get(id=user_id)
-#             user.is_online = online
-#             user.save()
-#         except User.DoesNotExist:
-#             pass
+    def update_user_status(self, user_id, online):
+        try:
+            user = User.objects.get(id=user_id)
+            user.is_online = online
+            user.save()
+        except User.DoesNotExist:
+            pass
 
-#     def receive(self, text_data):
-#         data = json.loads(text_data)
-#         user_id = data.get('userId')
-#         status = data.get('status')
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        user_id = data.get('userId')
+        status = data.get('status')
 
-#         if user_id and status:
-#             self.broadcast_status(user_id, status)
+        if user_id and status:
+            self.broadcast_status(user_id, status)
 
-#     def broadcast_status(self, user_id, status):
-#         channel_layer = get_channel_layer()
-#         async_to_sync(channel_layer.group_send)(
-#             'status_updates',
-#             {
-#                 'type': 'user_status',
-#                 'user_id': user_id,
-#                 'status': status
-#             }
-#         )
+    def broadcast_status(self, user_id, status):
+        channel_layer = get_channel_layer()
+        channel_layer.group_send(
+            'status_updates',
+            {
+                'type': 'user_status',
+                'user_id': user_id,
+                'status': status
+            }
+        )
 
-#     def user_status(self, event):
-#         self.send(text_data=json.dumps({
-#             'userId': event['user_id'],
-#             'status': event['status']
-#         }))
-
-
+    def user_status(self, event):
+        self.send(text_data=json.dumps({
+            'userId': event['user_id'],
+            'status': event['status']
+        }))
 
 
 
-from channels.consumer import AsyncConsumer
 
-class StatusConsumer(AsyncConsumer):
-
-    async def websocket_connect(self, event):
-        await self.send({
-            "type": "websocket.accept",
-        })
-
-    async def websocket_receive(self, event):
-        await self.send({
-            "type": "websocket.send",
-            "text": event["text"],
-        })
