ae950867b952dfc22806ceb7722066e1e3af9dd7
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-08-07 03:22:18 +0400

    asgiError

--- core/asgi.py
+++ core/asgi.py
@@ -1,12 +1,17 @@
+import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.sessions import SessionMiddlewareStack
 from .routing import websocket_urlpatterns
-from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         SessionMiddlewareStack(
             AuthMiddlewareStack(
