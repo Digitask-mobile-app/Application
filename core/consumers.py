a647fa8d1ac1bb7e3782e7a24858bb1b897b0e18
NicatDev <nicat254memmedov@gmail.com>
2024-08-07 01:36:09 +0400

    group_add

--- core/consumers.py
+++ core/consumers.py
@@ -9,15 +9,20 @@ class StatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
         self.user_id = self.scope['user'].id if self.scope['user'].is_authenticated else None
+        print('1')
         if self.user_id:
+            print('2')
             channel_layer = get_channel_layer()
             await channel_layer.group_add(
                 'status_updates',  
                 self.channel_name  
             )
+            print('3')
             StatusConsumer.online_users[self.user_id] = self.channel_name
             await self.update_user_status(self.user_id, True)
+            print('4')
             await self.broadcast_status(self.user_id, 'online')
+            print('5')
 
     async def disconnect(self, close_code):
         if self.user_id in StatusConsumer.online_users:
