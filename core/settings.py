83cc2b338052128b14cb37afeb9e238244fbdce5
NicatDev <nicat254memmedov@gmail.com>
2024-08-07 19:36:13 +0400

    tokenuser

--- core/settings.py
+++ core/settings.py
@@ -106,6 +106,9 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [('127.0.0.1', 6379)],
         },
+        'OPTIONS': {
+            'serializer': 'json',  # JSON ile serileştirme
+        },
     },
 }
 
