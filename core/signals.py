153cf926024a67718c2bf7c950aae8ad2b271457
NicatDev <nicat254memmedov@gmail.com>
2024-08-09 01:40:33 +0400

    checkanonimuser

--- core/signals.py
+++ core/signals.py
@@ -19,7 +19,7 @@ def user_status_update(sender, instance, **kwargs):
     async_to_sync(channel_layer.group_send)(
             'status',  # WebSocket grubu
             {
-                'type': 'send_users_custom',  # Consumer metodunu belirtir
+                'type': 'send.users',  # Consumer metodunu belirtir
                 'message': {'data': 'group send workinggggggggggggggggggggggggggggggg'}
             }
         )
