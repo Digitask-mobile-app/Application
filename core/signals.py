e15fb8fd8e5102dcda25848c23134374c948e98a
NicatDev <nicat254memmedov@gmail.com>
2024-08-08 22:51:24 +0400

    checkanonimuser

--- core/signals.py
+++ core/signals.py
@@ -2,14 +2,23 @@ from django.dispatch import receiver
 from asgiref.sync import async_to_sync
 from django.db.models.signals import post_save
 from accounts.models import User
-
+from channels.layers import get_channel_layer
 @receiver(post_save, sender=User)
 def user_status_update(sender, instance, **kwargs):
     from core.consumers import UserListConsumer
     print('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')
     print(kwargs,'-----------------------------------------------')
     print(kwargs.get('updated_fields'))
-    async_to_sync(UserListConsumer.send_users)({'message': 'data'})
+    channel_layer = get_channel_layer()
+
+    async_to_sync(channel_layer.group_send)(
+            'status',  # WebSocket grubu
+            {
+                'type': 'send_users',  # Consumer metodunu belirtir
+                'message': {'data': 'group send workinggggggggggggggggggggggggggggggg'}
+            }
+        )
+    print('group message senttttttttttttttttttt')
     if kwargs.get('update_fields') and 'is_online' in kwargs['update_fields']:
         online_users = User.objects.all().values('username', 'is_online')
         print(online_users)
