f7969b179175bf890971d31f62df17f6d9ffd671
NicatDev <nicat254memmedov@gmail.com>
2024-08-10 00:16:25 +0400

    consumerlastsendinguserlist

--- core/consumers.py
+++ core/consumers.py
@@ -171,7 +171,7 @@ class UserListConsumer(AsyncWebsocketConsumer):
     def get_online_users(self):
         from accounts.models import User
         print('getting online userssssssssss-----------------------------')
-        return [{user.id: 'online' if user.is_online else 'offline'} for user in User.objects.all()]
+        return [{str(user.id): 'online' if user.is_online else 'offline'} for user in User.objects.all()]
 
 
 
