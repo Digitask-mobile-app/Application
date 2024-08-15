b71ac89a6e69d4ddb765e7348671c96613c58d9c
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-08-16 02:38:10 +0400

    profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from ast import Expression
 from multiprocessing import context
 from django.shortcuts import render
@@ -15,15 +16,14 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from .serializers import UserSerializer
-from .filters import UserFilter,UserTypeFilter
+from .filters import UserFilter, UserTypeFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from django.db.models import Q 
+from django.db.models import Q
 
 from django.core.mail import send_mail
 from django.utils.crypto import get_random_string
 from django.db import IntegrityError
-
 
 
 class RegisterView(GenericAPIView):
@@ -48,47 +48,48 @@ class RegisterView(GenericAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class VerifyUserEmail(GenericAPIView):
     serializer_class = VerifyUserEmailSerializer
+
     def post(self, request):
         try:
             passcode = request.data.get('otp')
-            user_pass_obj=OneTimePassword.objects.get(otp=passcode)
-            user=user_pass_obj.user
+            user_pass_obj = OneTimePassword.objects.get(otp=passcode)
+            user = user_pass_obj.user
             if not user.is_verified:
-                user.is_verified=True
+                user.is_verified = True
                 user.save()
                 return Response({
-                    'message':'account email verified successfully'
+                    'message': 'account email verified successfully'
                 }, status=status.HTTP_200_OK)
-            return Response({'message':'passcode is invalid user is already verified'}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'message': 'passcode is invalid user is already verified'}, status=status.HTTP_204_NO_CONTENT)
         except OneTimePassword.DoesNotExist as identifier:
-            return Response({'message':'passcode not provided'}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response({'message': 'passcode not provided'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LoginUserView(GenericAPIView):
     serializer_class = LoginSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
 
 
 class PasswordResetRequestView(GenericAPIView):
     serializer_class = PasswordResetRequestSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
-        
-        email = serializer.validated_data.get('email') 
-        
+
+        email = serializer.validated_data.get('email')
+
         return Response({'message': 'OTP kodunu e-poçtunuza göndərdik.', 'email': email}, status=status.HTTP_200_OK)
 
-    
+
 class PasswordResetConfirm(GenericAPIView):
     def get(self, request, uidb64, token):
         try:
@@ -108,19 +109,22 @@ class VerifyOTPView(GenericAPIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
+
         return Response({'token': serializer.validated_data['token']}, status=status.HTTP_200_OK)
+
 
 class ResendOtpView(GenericAPIView):
     serializer_class = PasswordResetRequestSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
-        
+
         email = serializer.validated_data.get('email')
-        
+
         return Response({'message': 'Yeni OTP kodu e-poçtunuza göndərildi.'}, status=status.HTTP_200_OK)
+
 
 class SetNewPasswordView(GenericAPIView):
     serializer_class = SetNewPasswordSerializer
@@ -129,29 +133,32 @@ class SetNewPasswordView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        
+
         return Response({'message': 'Password has been reset successfully.'}, status=status.HTTP_200_OK)
 
 
 class TestingAuthenticatedReq(GenericAPIView):
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
 
-        data={
-            'msg':'its works'
+        data = {
+            'msg': 'its works'
         }
         return Response(data, status=status.HTTP_200_OK)
 
+
 class LogoutApiView(GenericAPIView):
-    serializer_class=LogoutUserSerializer
+    serializer_class = LogoutUserSerializer
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        serializer=self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
- 
+
+
 def delete_user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     if hasattr(user, 'onetimepassword'):
@@ -160,16 +167,16 @@ def delete_user(request, user_id):
     return HttpResponse("User and related OneTimePassword deleted successfully")
 
 
-from django.db import connection
-
-
 def delete_task(task_id):
     with connection.cursor() as cursor:
         cursor.execute("DELETE FROM myapp_task WHERE id = %s", [task_id])
 
+
 def update_auto_increment():
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE sqlite_sequence SET seq = (SELECT MAX(id) FROM myapp_task) WHERE name = 'myapp_task'")
+        cursor.execute(
+            "UPDATE sqlite_sequence SET seq = (SELECT MAX(id) FROM myapp_task) WHERE name = 'myapp_task'")
+
 
 class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
@@ -177,6 +184,12 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
 
 class UserListView(generics.ListAPIView):
@@ -187,7 +200,8 @@ class UserListView(generics.ListAPIView):
     search_fields = ['first_name', 'last_name']
 
     def get_queryset(self):
-        queryset = User.objects.filter(user_type__in=["Texnik", "Plumber", "Ofis menecer", "Texnik menecer"])
+        queryset = User.objects.filter(
+            user_type__in=["Texnik", "Plumber", "Ofis menecer", "Texnik menecer"])
 
         search_term = self.request.query_params.get('search', None)
         if search_term:
@@ -197,7 +211,8 @@ class UserListView(generics.ListAPIView):
             )
 
         return queryset
-    
+
+
 class UpdateUserView(generics.UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UpdateUserSerializer
@@ -210,7 +225,8 @@ class UpdateUserView(generics.UpdateAPIView):
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
-    
+
+
 class DeleteUserView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -218,7 +234,7 @@ class DeleteUserView(APIView):
         user = get_object_or_404(User, id=user_id)
         user.delete()
         return Response({"mesaj": "İstifadəçi uğurla silindi."}, status=status.HTTP_204_NO_CONTENT)
-    
+
 
 class UserFilterListView(generics.ListAPIView):
     queryset = User.objects.all()
