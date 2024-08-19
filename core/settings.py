91f1cccfd50255477db8423af3f213ffa6a73812
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-08-19 13:50:22 +0400

    page

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dj@g@p0zq76f6=wplo@znnuzclq4v=($p=ky@+x=$rx!9as@f2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").strip().lower() == "true"
 PROD = not DEBUG
 
 
