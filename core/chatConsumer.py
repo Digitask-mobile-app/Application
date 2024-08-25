d021aeb2524ec48648644050b47f730114c14c09
NicatDev <nicat254memmedov@gmail.com>
2024-08-25 14:37:22 +0400

    ChatConsumerTestingOnServer

--- core/chatConsumer.py
+++ core/chatConsumer.py
@@ -15,16 +15,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         user = self.scope['user']
 
         if user.is_authenticated:
+            print(user,'--------------------------------------chat')
             rooms = await database_sync_to_async(lambda: [x.name for x in user.rooms.all()])()
-
-            for room_name in rooms:
+            
+            for room_name in rooms: 
+                print(room_name,'--------------------------------------chat')
                 group_name = f'room_{room_name}'
                 await channel_layer.group_add(
                     group_name,
                     self.channel_name
                 )
         else:
-            print('user yoxdur')
+            print('user yoxdur --------------------------------------chat')
             await self.send(text_data=json.dumps({
                 'message': 'user auth deyil'
             }))
@@ -34,7 +36,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         channel_layer = get_channel_layer()
         user = self.scope['user']
-
+        print('exit from chat --------------------------------------chat')
         if user.is_authenticated:
             rooms = [x.name for x in user.rooms.all()]
             print(rooms)
@@ -51,7 +53,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         room_name = text_data_json['room']
         content = text_data_json['content']
         user = self.scope['user']
-        print(user,content,room_name)
+        print(user,content,room_name,'--------------------------------------chat')
         try:
             room = await database_sync_to_async(Room.objects.get)(name=room_name)
 
@@ -74,7 +76,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         content = event['content']
         user = event['user']
-
+    
         await self.send(text_data=json.dumps({
             'content': content,
             'user': user,
