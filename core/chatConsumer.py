3c5e566ae83e90f337658516e6a74ba735f40289
NicatDev <nicat254memmedov@gmail.com>
2024-08-28 04:29:22 +0400

    token

--- core/chatConsumer.py
+++ core/chatConsumer.py
@@ -39,7 +39,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         user = self.scope['user']
         print('exit from chat --------------------------------------chat')
         if user.is_authenticated:
-            rooms = [x.name for x in user.member_rooms.all()]
+            rooms = await database_sync_to_async(lambda: [x.name for x in user.member_rooms.all()])()
             print(rooms)
             for room_name in rooms:
                 group_name = f'room_{slugify(room_name)}'
