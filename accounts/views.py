72c74aaa419fa09abe0769908620a61871f18451
NicatDev <nicat254memmedov@gmail.com>
2024-10-09 21:32:00 +0400

    fu

--- accounts/views.py
+++ accounts/views.py
@@ -208,7 +208,7 @@ class ProfileView(generics.UpdateAPIView):
 
 class ProfileRetrieveView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = ProfileSerializer
+    serializer_class = ProfileSerializerForProfile
 
     def get_object(self):
         return self.request.user
