3d0cccb863472953b275cbb4e32d0617d0d53398
NicatDev <nicat254memmedov@gmail.com>
2024-10-13 17:46:18 +0400

    30 seconds

--- core/settings.py
+++ core/settings.py
@@ -238,7 +238,7 @@ else:
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
