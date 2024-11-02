496e0a8d505e70e23566a090190f6bc1fdddb739
NicatDev <nicat254memmedov@gmail.com>
2024-11-02 15:21:13 +0400

    'bulk_create'

--- core/settings.py
+++ core/settings.py
@@ -231,7 +231,7 @@ DATABASES = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=3),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
