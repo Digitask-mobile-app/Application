7ff9fc15d26980366c055d88e4a249efb20784c8
NicatDev <nicat254memmedov@gmail.com>
2024-11-12 20:50:03 +0400

    'days_7'

--- core/settings.py
+++ core/settings.py
@@ -231,7 +231,7 @@ DATABASES = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
