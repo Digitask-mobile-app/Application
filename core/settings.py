b6504e9aafc2473e85f3731258364022323da23a
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2025-02-08 13:39:04 +0400

    database

--- core/settings.py
+++ core/settings.py
@@ -135,14 +135,13 @@ if server_ip == '135.181.42.192':
 
         },
     }
-
+    
 else:
     CHANNEL_LAYERS = {
         'default': {
             'BACKEND': 'channels_redis.core.RedisChannelLayer',
             'CONFIG': {
-                # Redis'in çalıştığı host ve port
-                "hosts": [("127.0.0.1", 6380)],
+                "hosts": [("127.0.0.1", 6380)],  # Redis'in çalıştığı host ve port
             },
         },
     }
@@ -232,22 +231,14 @@ GDAL_LIBRARY_PATH = 'C:\\Program Files\\GDAL\\gdalxxx.dll'
 # print(server_ip,'------------------')
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'mydatabase'),
-#         'USER': os.getenv('POSTGRES_USER', 'digitask'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'Caspera-12345'),
-#         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-#         'PORT': os.getenv('POSTGRES_PORT', '5432'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        # Örneğin, SQLite kullanıyorsanız.
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Veritabanı dosyasının yolu.
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'mydatabase'),
+        'USER': os.getenv('POSTGRES_USER', 'digitask'), 
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'Caspera-12345'), 
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),  
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),  
     }
 }
 
@@ -367,4 +358,4 @@ CORS_ALLOW_HEADERS = [
 
 
 # django.setup()
-# 22
+#22
