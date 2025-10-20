fe8dd33f0a22730217610118ef2a56acc4be3981
NicatDev <nicat254memmedov@gmail.com>
2025-10-20 19:16:34 +0400

    main

--- core/settings.py
+++ core/settings.py
@@ -345,7 +345,7 @@ else:
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_URL = 'https://app.desgah.az/media/'
+MEDIA_URL = 'https://app.digitask.store/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
